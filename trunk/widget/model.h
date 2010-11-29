5f7b0dc1504034d842050eb0e4f5310423ad660c
lipixx@gmail.com <lipixx@gmail.com@5fff5b5c-4a61-0d60-91d8-25d46f205a8a>
2010-11-29 10:54:11 +0000

    + Modificat model.cpp i initializeGL(). La ilu. va funcionant, i ara s'agafa el material dels objectes carregats amb readObj();

--- trunk/widget/model.h
+++ trunk/widget/model.h
@@ -11,53 +11,35 @@
   
 using namespace std;
 
-
 // Model - permet representar un objecte 3D
-  class Model 
+class Model
 {
-
 public:
 Model (std::string);
-  
 
-    // llegeix un model en format OBJ
-  void readObj (const char *filename, MaterialLib & matlib);
-  
+// llegeix un model en format OBJ
+void readObj (const char *filename, MaterialLib & matlib);
+// calcula la capsa del model
+void updateBoundingBox ();
+// retorna la capsa del model (previament s'ha d'haver calculat)
+Box boundingBox ()const;
 
-    // calcula la capsa del model
-  void updateBoundingBox ();
-  
-
-    // retorna la capsa del model (previament s'ha d'haver calculat)
-    Box boundingBox ()const;
-  
-
-void Render (GLenum mode, bool seleccionant, int idObj);
-  
+void Render (GLenum mode, bool seleccionant, int idObj);  
 std::string getName ();
 
-
 private:
-void make_face (char **words, int nwords, int material);
-  
+void make_face (char **words, int nwords, int material); 
 void netejaDades ();
-  
 void computeNormals ();
 
-
 public:
-vector < Vertex > vertices;
-				// vector amb els vertexs de l'objecte
-  vector < Face > faces;	// vector amb les cares de l'objecte
+vector < Vertex > vertices;   // vector amb els vertexs de l'objecte
+vector < Face > faces;	// vector amb les cares de l'objecte
 
 private:
 std::string name;
-  
 Box _boundingBox;		// caixa contenidora de l'objecte.
 };
-
-
-
 
 #endif	/* 
  */
